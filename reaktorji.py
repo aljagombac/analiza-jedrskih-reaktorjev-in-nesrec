1402dc3e0f6bb711525ba1f58bbf9abd27fdb37b
Alja Gombač <alja.gombac@gmail.com>
2024-07-27 20:04:44 +0200

    uredila kodo za pridobivanje csv-jev

--- reaktorji.py
+++ reaktorji.py
@@ -69,15 +69,25 @@ def preberi_dat_v_niz(mapa, datoteka):
         vsebina = dat.read()
     return vsebina
 
-def podatki_v_csv(vsebina_strani, dat_csv, mapa):
-    """Poišče tabele s podatki o potresih posamezne html strani in podatke napiše v csv datoteko."""
-    soup = BeautifulSoup(vsebina_strani, "html.parser")
-    pot = os.path.join(mapa,dat_csv)
+def poisci_tabele(vsebina_strani, soup):
+    """Iz spletne strani izlušči tabele s podatki o jedrskih reaktorjih za posamezno državo."""    
     sez_tabel = soup.select("table.wikitable.sortable")
+    return sez_tabel
+
+def poisci_drzave(vsebina_strani, soup):
+    """Iz spletne strani izlušči imena državz z jedrskimi reaktorji."""
     nepotrebni_headers = ["Contents", "Overview", "Nuclear safety"]
-    sez_drzav = [header.get_text() for header in soup.select("h2") if not header.get_text() in nepotrebni_headers]
-    
-    tabela_drzava = zip(sez_tabel, sez_drzav)    
+    sez_drzav = []
+    for header in soup.select("h2"):
+        if not header.get_text() in nepotrebni_headers:
+            sez_drzav.append(header.get_text())    
+    return sez_drzav
+
+def zapisi_tabele_v_csv(tabele, drzave, dat_csv, mapa):
+    """Podatke o jedrskih reaktorjih iz tabel zapiše v csv datoteko in vsaki tabeli doda ime države, kateri pripadajo reaktorji."""
+    pot = os.path.join(mapa,dat_csv)
+
+    tabela_drzava = zip(tabele, drzave)    
     for i, (tabela, drzava) in enumerate(tabela_drzava):
         df = pd.read_html(StringIO(str(tabela)))        
         df = pd.concat(df)
@@ -85,16 +95,46 @@ def podatki_v_csv(vsebina_strani, dat_csv, mapa):
         if i == 0:     
             df.to_csv(pot, mode="w", index=False)
         else:
-            df.to_csv(pot, mode="a", index=False)
+            df.to_csv(pot, mode="a", index=False)   
 
+
+#def podatki_v_csv(vsebina_strani, dat_csv, mapa):
+#    """Poišče tabele s podatki o reaktorjih in jih napiše v csv datoteko."""
+#    soup = BeautifulSoup(vsebina_strani, "html.parser")
+#    pot = os.path.join(mapa,dat_csv)
+#    sez_tabel = soup.select("table.wikitable.sortable")
+#    nepotrebni_headers = ["Contents", "Overview", "Nuclear safety"]
+#    sez_drzav = [header.get_text() for header in soup.select("h2") if not header.get_text() in nepotrebni_headers]
+#    
+#    tabela_drzava = zip(sez_tabel, sez_drzav)    
+#    for i, (tabela, drzava) in enumerate(tabela_drzava):
+#        df = pd.read_html(StringIO(str(tabela)))        
+#        df = pd.concat(df)
+#        df.rename(columns=({"Plant name": "Plant name" + " - " + drzava, "Location" : "Location" + " - " + drzava}), inplace=True)   
+#        if i == 0:     
+#            df.to_csv(pot, mode="w", index=False)
+#        else:
+#            df.to_csv(pot, mode="a", index=False)
 
 def main():
+    
     vsebina1 = preberi_dat_v_niz(reaktorji_mapa, reaktorji_html)
     vsebina2 = preberi_dat_v_niz(reaktorji_mapa, nesrece_html)
     print("Shranil vsebino obeh html-jev v niz.")
 
-    podatki_v_csv(vsebina1, reaktorji_csv, reaktorji_mapa)
-    podatki_v_csv(vsebina2, nesrece_csv, reaktorji_mapa)
+    soup1 = BeautifulSoup(vsebina1, "html.parser")
+    soup2 = BeautifulSoup(vsebina2, "html.parser")
+
+    tabele1 = poisci_tabele(vsebina1, soup1)
+    tabele2 = poisci_tabele(vsebina2, soup2)
+    print("Uspešno poiskal tabele.")
+
+    drzave1 = poisci_drzave(vsebina1, soup1)
+    drzave2 = poisci_drzave(vsebina2, soup2)
+    print("Uspešno poiskal imena držav.")
+
+    zapisi_tabele_v_csv(tabele1, drzave1, reaktorji_csv, reaktorji_mapa)
+    zapisi_tabele_v_csv(tabele2, drzave2, nesrece_csv, reaktorji_mapa)
     print("Uspešno shranil csv-ja.")
 
     print("Konec!")
