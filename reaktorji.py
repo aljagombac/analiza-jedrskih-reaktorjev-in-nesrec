0684af934825c1633e89d7765c27a54f1cf48b27
Alja Gombač <alja.gombac@gmail.com>
2024-08-20 14:57:48 +0200

    zdruzila main funkciji

--- reaktorji.py
+++ reaktorji.py
@@ -1,8 +1,6 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-import re
-import numpy as np
 import pandas as pd
 from io import StringIO
 
@@ -12,13 +10,13 @@ reaktorji_url = "https://en.wikipedia.org/wiki/List_of_commercial_nuclear_reacto
 nesrece_url = "https://en.wikipedia.org/wiki/List_of_nuclear_power_accidents_by_country"
 # mapa, kjer sta shranjeni html datoteki
 reaktorji_mapa = "podatki"
-#datoteka kamor se shrani html o jedrskih reaktorjih
+# datoteka kamor se shrani html o jedrskih reaktorjih
 reaktorji_html = "jedrski-reaktorji.html"
-#datoteka kamor se shrani html o jedrskih nesrečah
+# datoteka kamor se shrani html o jedrskih nesrečah
 nesrece_html = "jedrske-nesrece.html"
-#datoteka, v katero se shrani csv o jedrskih reaktorjih posameznih držav
+# datoteka, v katero se shrani csv o jedrskih reaktorjih posameznih držav
 reaktorji_csv = "jedrski-reaktorji.csv"
-#datoteka, v katero se shrani csv o jedrskih nesrečah
+# datoteka, v katero se shrani csv o jedrskih nesrečah
 nesrece_csv = "jedrske-nesrece.csv"
 
 def shrani_url_v_niz(url):
@@ -44,23 +42,6 @@ def shrani_html(url, mapa, datoteka):
     """Shrani niz url-ja v html datoteko."""
     text = shrani_url_v_niz(url)
     shrani_niz_v_datoteko(text, mapa, datoteka)
-
-def main():
-    url1 = reaktorji_url
-    url2 = nesrece_url
-
-    text1 = shrani_url_v_niz(url1)
-    text2 = shrani_url_v_niz(url2)
-    print("Shranil oba url-ja v niz.")
-
-    shrani_html(url1, reaktorji_mapa, reaktorji_html)
-    shrani_html(url2, reaktorji_mapa, nesrece_html)
-    print("Uspesno shranil html-ja.")
-
-    print("Konec!")
-
-#if __name__ == "__main__":
-#  main()
 
 def preberi_dat_v_niz(mapa, datoteka):
     """Vrne celotno vsebino datoteke kot niz."""
@@ -151,6 +132,12 @@ def zdruzi_in_zapisi_v_csv(sez_df, dat_csv, mapa):
     df_vse_tabele.to_csv(pot, mode="w", index=False)
 
 def main():
+    url1 = reaktorji_url
+    url2 = nesrece_url
+
+    shrani_html(url1, reaktorji_mapa, reaktorji_html)
+    shrani_html(url2, reaktorji_mapa, nesrece_html)
+    print("Uspesno shranil html-ja.")
     
     vsebina1 = preberi_dat_v_niz(reaktorji_mapa, reaktorji_html)
     vsebina2 = preberi_dat_v_niz(reaktorji_mapa, nesrece_html)
